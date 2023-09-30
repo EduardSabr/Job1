d6935e825094faa20302ee394a6097c3a77bc17f
EduardSabr <eduard.sabrekov04@gmail.com>
2023-09-30 23:16:42 +0500

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,24 @@
+a = int(input('Введи число a: '))
+b = int(input('Введи число b: '))
+print(a + b)
+
+# разность
+a = int(input('Введи число a: '))
+b = int(input('Введи число b: '))
+print(a - b)
+
+# произведение
+a = int(input('Введи число a: '))
+b = int(input('Введи число b: '))
+print(a * b)
+
+
+# среднее арифметическое
+a = int(input('Введи число a: '))
+b = int(input('Введи число b: '))
+print((a + b)/2)
+
+# разность макс. и мин. модулей
+a = int(input('Введи число a: '))
+b = int(input('Введи число b: '))
+print(max(abs(a),abs(b)) - min(abs(a),abs(b)))
