8ca3bb72424950d40478cc9649b9c0ed14130af5
EduardSabr <146577127+EduardSabr@users.noreply.github.com>
2023-09-30 23:55:10 +0500

    Добавление частных чисел с точностью до сотых

--- 1.py
+++ 1.py
@@ -1,24 +1,25 @@
 a = int(input('Введи число a: '))
 b = int(input('Введи число b: '))
-print(a + b)
+print(a + b,,round(a/b,2))
+
 
 # разность
 a = int(input('Введи число a: '))
 b = int(input('Введи число b: '))
-print(a - b)
+print(a - b,,round(a/b,2))
 
 # произведение
 a = int(input('Введи число a: '))
 b = int(input('Введи число b: '))
-print(a * b)
+print(a * b,,round(a/b,2))
 
 
 # среднее арифметическое
 a = int(input('Введи число a: '))
 b = int(input('Введи число b: '))
-print((a + b)/2)
+print((a + b)/2,,round(a/b,2))
 
 # разность макс. и мин. модулей
 a = int(input('Введи число a: '))
 b = int(input('Введи число b: '))
-print(max(abs(a),abs(b)) - min(abs(a),abs(b)))
+print(max(abs(a),abs(b)) - min(abs(a),abs(b),,round(a/b,2))
